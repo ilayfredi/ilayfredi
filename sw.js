432b2f87943ea226af08f3462df5325c3e1328d5
ilayfredi <ilay.fredi@gmail.com>
2026-08-19 17:10:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Orbit Decay service worker.
    The whole game is one HTML file, so a plain precache makes it work
    offline and satisfies the installability requirement. */
-var VERSION = "od-v5";
+var VERSION = "od-v6";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
